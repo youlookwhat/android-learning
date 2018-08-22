63fa8f828a2c459063b3dff4c0fb3b13e1075086
youlookwhat <770413277@qq.com>
2018-08-22 16:02:15 +0800

    lottie 新增几种动画

--- LottieDemo/app/src/main/java/jing/cn/lottiedemo/MainActivity.java
+++ LottieDemo/app/src/main/java/jing/cn/lottiedemo/MainActivity.java
@@ -1,7 +1,9 @@
 package jing.cn.lottiedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // https://juejin.im/post/5b0bc72c6fb9a00a0a4d5a4f
+        final LottieAnimationView viewById = findViewById(R.id.animation_view);
+
+//        viewById.addAnimatorListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                Toast.makeText(MainActivity.this, "结束", Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
     }
 }
